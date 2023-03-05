04b28f7fbdb4ae5fd0295a198d45ab5ef5c9b529
Andrés Naves <andnama@gamil.com>
2023-03-05 19:21:42 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <body>
+    <div class= "justify-center flex bg-black h-screen w-screen">
+      <h1 class="font-normal absolute h-3 text-white"> Hello </h1>
+    </div>
+  </body>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
